9eff7b1fa20373647d07abf3e10d5deab890b20a
SGrinstead <seth.grinstead3299@gmail.com>
2023-04-17 09:54:30 -0700

    Refactoring Lesson added dog class

---
+++ DogSitter2/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogSitter
+{
+    public class Dog
+    {
+        public string Name;
+        public string Breed;
+        public string Birthday;
+        public int WalksRequired;
+
+        public Dog(string name, string breed, string birthday, int walksRequired)
+        {
+            Name = name;
+            Breed = breed;
+            Birthday = birthday;
+            WalksRequired = walksRequired;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine($"Taking {Name} for a walk...");
+            WalksRequired--;
+            Console.WriteLine($"{Name} now needs {WalksRequired} walks.");
+        }
+    }
+}
--- DogSitter2/Program.cs
+++ DogSitter2/Program.cs
@@ -4,34 +4,16 @@
     {
         static void Main()
         {
-            var nile = new Dictionary<string, string>
-            {
-                { "name", "Nile" },
-                { "breed", "Golden Mix"},
-                { "birthday", "2018-07-04"},
-                { "walks required", "3" }
-            };
+            var nile = new Dog("Nile", "Golden Mix", "2018-07-04", 3);
+            var sammy = new Dog("Sammy", "Wirehaired Pointing Griffon", "2012-10-13", 1);
 
-            var sammy = new Dictionary<string, string>
-            {
-                { "name", "Sammy" },
-                { "breed", "Wirehaired Pointing Griffon" },
-                { "birthday", "2012-10-13" },
-                { "walks required", "1" }
-            };
-
-            var dogs = new List<Dictionary<string, string>> { nile, sammy };
+            var dogs = new List<Dog> { nile, sammy };
 
             // Take dogs on a walk
             foreach (var dog in dogs)
             {
-                var currentWalksRequired = int.Parse(dog["walks required"]);
-                Console.WriteLine($"Taking {dog["name"]} for a walk...");
-                dog["walks required"] = (currentWalksRequired - 1).ToString();
+                dog.Walk();
             }
-
-            Console.WriteLine($"{nile["name"]} now needs {nile["walks required"]} walks");
-            Console.WriteLine($"{sammy["name"]} now needs {sammy["walks required"]} walks");
         }
     }
 }
